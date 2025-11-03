0518cbccaa7a381e4f9e684b6476048f260af14d
ThilinaSupunsara <thilinasupunsara2001@gmail.com>
2025-11-03 13:54:53 +0530

    back

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,19 +1,12 @@
 #!/bin/sh
+
+# Exit immediately if a command fails
 set -e
 
-# This entrypoint script will run for all services (web, reverb, queue)
-# We use the passed-in command ($1) to determine which service this is.
 
-if [ "$1" = "apache2-foreground" ]; then
-    # This is the WEB service
-    echo "Running as WEB service. Caching config and running migrations..."
-    php artisan config:cache
-    php artisan route:cache
-   
-else
-    # This is a WORKER or REVERB service
-    echo "Running as WORKER/REVERB service. Skipping setup."
-fi
+# Cache configuration for performance
+php artisan config:cache
+php artisan route:cache
 
-# Execute the command specified in render.yaml (e.g., "apache2-foreground")
-exec "$@"
+# Start the Apache web server
+exec apache2-foreground
